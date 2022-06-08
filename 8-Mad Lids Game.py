3cbbed89bf70b661347e576472494b6719ce954a
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-06-08 22:38:11 +0200

    Create 8-Mad Lids Game.py

---
+++ 8-Mad Lids Game.py
@@ -0,0 +1,13 @@
+#Creating a mad libs game
+print("Roses are red")
+print("Violets are blue")
+print("I love you")
+
+#Change red to user inputted color, violetes to a plural noun, you to celebrity
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ") 
+celebrity = input("Enter a celebrity name: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
