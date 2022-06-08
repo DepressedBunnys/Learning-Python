0f36e8a2d3eb5c957ea65650b8bfd48aabc7dbe8
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-06-08 21:54:39 +0200

    Create 3-variables.py

---
+++ 3-variables.py
@@ -0,0 +1,15 @@
+# Basic Data types
+# String = plain text EG. "Hello"
+# Numbers = number EG. 50, 50.90
+# Boolean = true/false value EG. is_female = True/False
+
+#Creating variables
+character_name = "John" #Character name has the value John
+character_age = 35
+print("There was once a man named " + character_name + ", ") #Adding the character name to the string
+print("he was " + character_age + " years old. ")
+
+character_name = "Tom" #Giving a different value to the character name variable
+character_age = 50
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
