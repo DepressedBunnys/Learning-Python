d89e0c47d35bd000859d781d4d1cc4701c0070f7
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-06-08 22:20:09 +0200

    Create 6-inputs.py

---
+++ 6-inputs.py
@@ -0,0 +1,6 @@
+# This prompts the user to enter their name
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+# here we are assigning the name to the user_name variable
+print("Hello " + user_name)
+print("Age " + user_age)
