f28e38e8d45a683d27b3ec68746b192e0b1bc969
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-06-08 22:11:49 +0200

    Create 5-numbers.py

---
+++ 5-numbers.py
@@ -0,0 +1,40 @@
+print(20)
+print(2.0050)
+print(-2.0050)
+print(3 + 4.5) #You can do everything: +,-,*,/
+print((4 + 5) * 3) #Will use BIDMAS rules
+print(10 % 3) #10 MOD 3, will get remainder 
+my_number = 18 #Storing 18 to my_number
+print(my_number)
+print(str(my_number)) #Changes the number to a string
+print("I am " + str(my_number) + " years old.") #Will return an error if we try to print a number next
+#to a string so we do this
+print(abs(-5)) #Gives the absolute value of the number, gives 5
+print(pow(2, 2)) #First parameter is the number, second paramter is the exponential that we want to add 
+#gives 4
+print(max(4, 6)) #Returns the larger number, min does the opposite
+print(round(3.2)) #Rounds using normal rouding rules
+
+#Importing in Python
+from math import * #Gets the math functions
+
+print(floor(3.7)) #Rounds down, gives 3
+print(ceil(3.7)) #Always rounds up, gives 4
+print(sqrt(4)) #Returns the square root of a number
+
+#20
+#2.005
+#-2.005
+#7.5
+#27
+#1
+#18
+#18
+#I am 18 years old.
+#5
+#4
+#6
+#3
+#3
+#4
+#2.0
