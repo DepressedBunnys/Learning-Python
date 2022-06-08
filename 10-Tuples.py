57cfbd4ca920f05b40396e58a4714def3a60852e
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-06-08 23:10:56 +0200

    Create 10-Tuples.py

---
+++ 10-Tuples.py
@@ -0,0 +1,6 @@
+coordinates = (5, 7)
+#These are immutable, they can't be changed or modified
+print(coordinates[0]) #Returns 5, just like strings tuples's index starts from 0
+
+#Difference between a tuple and a list
+#List: Can change, Tuple: Cannot Change
